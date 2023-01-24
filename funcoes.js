8211851f25bc40e40bc0a0e05de4d2963829d2f8
Werbsson Saymon <64561641+WerbssonSaymon@users.noreply.github.com>
2023-01-24 11:35:03 -0300

    Envio de arquivos

---
+++ funcoes.js
@@ -0,0 +1,8 @@
+
+	var Npreco = document.querySelector('#preco')
+	var Nquant = document.querySelector('#quant')
+	var Resultado = document.querySelector('#result')
+
+function Calculo() {
+	Resultado.getElementById('#result') = parseInt(Npreco.value) * parseInt(Nquant.value) 
+}
